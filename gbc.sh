c8f4dcfc97046c2d601820c64cd54e9b946dcf95
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-12-22 23:38:42 -0800

    Update gbc.sh

--- gbc.sh
+++ gbc.sh
@@ -5,8 +5,8 @@ echo "Dirbust Controller v0.0.1"
 echo "Killing previous instances of gobuster"
 killall gobuster &
 
-if [ ! -f "targets" ]; then
-        echo "Create a targets file with url(s) seperated by new lines to run"
+if [ ! -f "domains" ]; then
+        echo "Create a `domains` file with full url(s) seperated by new lines to run your campaigns against"
         exit
 fi
 
@@ -14,7 +14,7 @@ if [ ! -d "gobuster-results" ]; then
         mkdir gobuster-results
 fi
 
-for item in $(cat targets) ; do
+for item in $(cat domains) ; do
         filename=$( echo $item | cut -d "/" -f3- );
         echo "gobuster-results/$filename"
         gobuster -w /usr/share/wordlists/fuzzdb/discovery/predictable-filepaths/filename-dirname-bruteforce/raft-large-directories-lowercase.txt -u $item -t 100 > gobuster-results/$filename &
