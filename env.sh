73b44cd69862064662869e79300463827161e269
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-12-26 04:05:37 -0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -10,31 +10,36 @@ if [ -d "$name" ]; then
   exit
 fi
 
+if [ ! -f "targets" ]; then
+  echo "You must create a targets file"
+  exit
+fi
+
 mkdir $name
-mkdir $name/Phase-1
-mkdir $name/Phase-1/nmap-results
-touch $name/Phase-1/targets
-mkdir $name/Phase-2
-mkdir $name/Phase-2/gobuster-results
-touch $name/Phase-2/domains
+mkdir $name/results
+mkdir $name/results/nmap
+cp tools/parse-nmap.sh $name/results/parse-nmap.sh
+chmod +x $name/results/*.sh
 
-curl -o $name/Phase-2/gbc.sh https://raw.githubusercontent.com/dostoevskylabs/engagement-scripts/master/tools/gbc.sh 2>/dev/null
-chmod +x $name/Phase-2/gbc.sh
+for target in $(cat targets); do
+	if [ ! -d "$name/$target" ]; then
+		mkdir $name/$target
+		mkdir $name/$target/Phase-1
+		mkdir $name/$target/Phase-2
+    cp tools/dirfuzz.sh $name/$target/Phase-2/dirfuzz.sh
+    chmod +x $name/$target/Phase-2/*.sh
+	fi
 
-echo "nmap -vv -sF -Pn -iL targets -oN nmap-results/nmap-F" > $name/Phase-1/xxx-nmapF
-echo "nmap -vv -sn --open -iL targets -oN nmap-results/pingsNmap" > $name/Phase-1/xxx-nmapP
-echo "nmap -vv -sT -Pn -p 1025-65535 -iL targets -oN nmap-results/nmap-T-full" > $name/Phase-1/xxx-nmapTfull
-echo "nmap -vv -sT -Pn -p 1-1024 -iL targets -oN nmap-results/nmap-T-priv" > $name/Phase-1/xxx-nmapTpriv
-echo "nmap -vv -sU -Pn -p 1-1024 -iL targets -oN nmap-results/nmap-U-priv" > $name/Phase-1/xxx-nmapUpriv
+	echo "nmap -vv -sF -Pn --append-output -oG ../../results/nmap/$target $target" > $name/$target/Phase-1/xxx-nmapF
+	echo "nmap -vv -sn --open --append-output -oG ../../results/nmap/$target $target" > $name/$target/Phase-1/xxx-nmapP
+	echo "nmap -vv -sT -Pn -p 1025-65535 --append-output -oG ../../results/nmap/$target $target" > $name/$target/Phase-1/xxx-nmapTfull
+	echo "nmap -vv -sT -Pn -p 1-1024 --append-output -oG ../../results/nmap/$target $target" > $name/$target/Phase-1/xxx-nmapTpriv
+	echo "nmap -vv -sU -Pn -p 1-1024 --append-output -oG ../../results/nmap/$target $target" > $name/$target/Phase-2/xxx-nmapUpriv
 
-echo "./xxx-nmapP" >> $name/Phase-1/xxx-master.sh
-echo "./xxx-nmapF" >> $name/Phase-1/xxx-master.sh
-echo "./xxx-nmapTpriv" >> $name/Phase-1/xxx-master.sh
-echo "./xxx-nmapUpriv" >> $name/Phase-1/xxx-master.sh
-echo "./xxx-nmapTfull" >> $name/Phase-1/xxx-master.sh
-chmod +x $name/Phase-1/xxx-*
-
-echo
-echo "Environment created for $name"
-echo "Begin Phase-1 by adding targets and running $name/Phase-1/xxx-master.sh"
-echo "Begin Phase-2 by adding domains and running $name/Phase-2/gbc.sh"
+	echo "./xxx-nmapP" > $name/$target/Phase-1/xxx-master.sh
+	echo "./xxx-nmapF" >> $name/$target/Phase-1/xxx-master.sh
+	echo "./xxx-nmapTpriv" >> $name/$target/Phase-1/xxx-master.sh
+	echo "./xxx-nmapUpriv" >> $name/$target/Phase-1/xxx-master.sh
+	echo "./xxx-nmapTfull" >> $name/$target/Phase-1/xxx-master.sh
+	chmod +x $name/$target/Phase-1/xxx-*
+done
