8bb66dfe36e49494cba92b26323bbbbaf9fbb9e7
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2018-01-07 18:08:33 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -32,27 +32,30 @@ chmod +x $name/Phase-1/dirbust/*.sh
 mkdir $name/Phase-2
 mkdir $name/Phase-2/exploits
 mkdir $name/Phase-2/http-reqres
+cp tools/linuxfiles.txt $name/Phase-2/linuxfiles.txt
 
 mkdir $name/Phase-3
 
 mkdir $name/Phase-3/shells
 cp tools/tcpReverseShell.py $name/Phase-3/shells/tcpReverseShell.py
 cp tools/udpReverseShell.py $name/Phase-3/shells/udpReverseShell.py
+echo "socat file:`tty`,echo=0,raw udp-listen:443" > $name/Phase-3/socatListener.sh
+echo "python -m SimpleHTTPServer" > $name/Phase-3/httpServer.sh
+chmod +x $name/Phase-3/*.sh
 
 mkdir $name/Phase-3/privesc
+cp tools/setuid.c  $name/Phase-3/privesc/setuid.c
 cp tools/linuxprivchecker.py $name/Phase-3/privesc/linuxprivchecker.py
 
 touch $name/Phase-1/nmap/targets
 touch $name/Phase-1/dirbust/domains
 
-echo "nmap -vv -sF -iL targets -Pn --append-output -oG ../../results/nmap/nmapF" > $name/Phase-1/nmap/xxx-nmapF
-echo "nmap -vv -sn -iL targets --open --append-output -oG ../../results/nmap/nmapP" > $name/Phase-1/nmap/xxx-nmapP
-echo "nmap -vv -sT -iL targets -Pn -p 1025-65535 --append-output -oG ../../results/nmap/nmapTfull" > $name/Phase-1/nmap/xxx-nmapTfull
-echo "nmap -vv -sT -iL targets -Pn -p 1-1024 --append-output -oG ../../results/nmap/nmapTpriv" > $name/Phase-1/nmap/xxx-nmapTpriv
-echo "nmap -vv -sU -iL targets -Pn -p 1-1024 --append-output -oG ../../results/nmap/nmapUpriv" > $name/Phase-1/nmap/xxx-nmapUpriv
+echo "nmap -vv -n --open --reason -PS21,22,23,25,135,139,445,3389,1433,80,443,8080,8443,90,3268,110,53,3306,1723,111,995,993,5900,1025,1720,465,548,5060,8000,515,2049,6000,389,5432 -iL targets --append-output -oG ../../results/nmap/nmapCommPorts" > $name/Phase-1/nmap/xxx-nmapCommPorts
+echo "nmap -vv -sT --open --reason -R -T4 --max-retries 3 --min-rate 120 --max-rtt-timeout 300ms -Pn -p 1025-65535 -iL targets --append-output -oG ../../results/nmap/nmapTfull" > $name/Phase-1/nmap/xxx-nmapTfull
+echo "nmap -vv -sT --open --reason -R -T4 --max-retries 3 --min-rate 120 --max-rtt-timeout 300ms -Pn -p 1-1024 -iL targets --append-output -oG ../../results/nmap/nmapTpriv" > $name/Phase-1/nmap/xxx-nmapTpriv
+echo "nmap -vv -sU --open --reason -R -T4 --max-retries 3 --min-rate 120 --max-rtt-timeout 300ms -Pn -p 1-1024 -iL targets --append-output -oG ../../results/nmap/nmapUpriv" > $name/Phase-1/nmap/xxx-nmapUpriv
 
-echo "./xxx-nmapP" > $name/Phase-1/nmap/xxx-master.sh
-echo "./xxx-nmapF" >> $name/Phase-1/nmap/xxx-master.sh
+echo "./xxx-nmapCommPorts" > $name/Phase-1/nmap/xxx-master.sh
 echo "./xxx-nmapTpriv" >> $name/Phase-1/nmap/xxx-master.sh
 echo "./xxx-nmapUpriv" >> $name/Phase-1/nmap/xxx-master.sh
 echo "./xxx-nmapTfull" >> $name/Phase-1/nmap/xxx-master.sh
