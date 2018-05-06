1173ae47e3408d6f5f28cfca81649438b81ceddf
Elijah <dostoevskylabs@users.noreply.github.com>
2018-05-05 21:25:34 -0400

    Update dirsearch.sh

--- tools/dirsearch.sh
+++ tools/dirsearch.sh
@@ -1,6 +1,5 @@
-#if [ ! -x "/usr/bin/dirsearch" ]; then
-#  echo "dirsearch not found in /usr/bin" >&2
-#  exit
-#fi
-#dirsearch -L domains $@ --json-report="../../results/dirsearch/output.json"
-echo $@
+if [ ! -x "/usr/bin/dirsearch" ]; then
+  echo "dirsearch not found in /usr/bin" >&2
+  exit
+fi
+dirsearch -L domains $@ --json-report="../../results/dirsearch/output.json"
